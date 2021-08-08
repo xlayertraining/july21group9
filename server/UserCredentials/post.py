ad62d6888a401d38364a7b936797c43eec2a032f
Nabanit31 <nabanitroy516@gmail.com>
2021-08-08 12:03:43 +0530

    get and delete api image is done
    Signed-off-by: Nabanit31 <nabanitroy516@gmail.com>

--- server/UserCredentials/post.py
+++ server/UserCredentials/post.py
@@ -1,5 +1,5 @@
 from mimetypes import MimeTypes
-from common_library import*
+from common_library import *
 
 
 class imageHandler(tornado.web.RequestHandler):
@@ -27,13 +27,13 @@ class imageHandler(tornado.web.RequestHandler):
                 message = "Couldn't get the type name"
                 raise Exception
             try:
-                postTime=str(self.request.arguments["time"][0].decode())
-                if postTime==None or postTime=='':
+                postTime = str(self.request.arguments["time"][0].decode())
+                if postTime == None or postTime == '':
                     raise Exception
             except:
-                code=8044
-                status=False
-                message="Time is invalid"
+                code = 8044
+                status = False
+                message = "Time is invalid"
                 raise Exception
             try:
                 fileType = str(mimetypes.guess_extension(
@@ -66,3 +66,91 @@ class imageHandler(tornado.web.RequestHandler):
 
         except:
             self.write(message)
+
+    async def get(self):
+        code = 4000
+        status = False
+        message = ""
+        result = []
+        try:
+            imageList = user_image_folder.find()
+            async for i in imageList:
+                i['_id'] = str(i['_id'])
+                result.append(i)
+            code = 2000
+            status = True
+            message = "List of images"
+        except:
+            status = False
+            if not len(message):
+                message = 'Internal Error, Please Contact the Support Team.'
+        response = {
+            'code': code,
+            'status': status,
+            'message': message
+        }
+        try:
+            response['result'] = result
+            self.write(response)
+            self.finish()
+            return
+        except Exception as e:
+            status = False
+            code = 5011
+            message = 'Internal Error, Please Contact the Support Team.'
+            response = {
+                'code': code,
+                'status': status,
+                'message': message
+            }
+            self.write(response)
+            self.finish()
+            return
+
+    async def delete(self):
+        code = 4000
+        status = False
+        message = ""
+        result = []
+        try:
+            try:
+                imageId = ObjectId(self.request.arguments["id"][0].decode())
+            except:
+                code = 3000
+                status = False
+                message = "invalid image id"
+                raise Exception
+            proUpdate = user_image_folder.delete_one(
+                {
+                    "_id": imageId
+                },
+            )
+            code = 2000
+            status = True
+            message = "Image has been removed"
+        except:
+            status = False
+            if not len(message):
+                message = 'Internal Error, Please Contact the Support Team.'
+        response = {
+            'code': code,
+            'status': status,
+            'message': message
+        }
+        try:
+            response['result'] = result
+            self.write(response)
+            self.finish()
+            return
+        except Exception as e:
+            status = False
+            code = 5011
+            message = 'Internal Error, Please Contact the Support Team.'
+            response = {
+                'code': code,
+                'status': status,
+                'message': message
+            }
+            self.write(response)
+            self.finish()
+            return
