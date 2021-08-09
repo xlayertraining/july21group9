f9a3bf38f25a3e94f2f86172f89950a040108a8c
Aniket_lodh <aniket.lodh.08@gmail.com>
2021-08-09 09:44:31 +0530

    Note* Refactored the POST,GET,DELETE API's Added JWT tokens to user sign-in and sign-up files Signed-off-by: Aniket_lodh <aniket.lodh.08@gmail.com>

--- server/UserCredentials/post.py
+++ server/UserCredentials/post.py
@@ -15,7 +15,7 @@ class imageHandler(tornado.web.RequestHandler):
         type = ""
         try:
             account_id = await SecureHeader.decrypt(self.request.headers["Authorization"])
-            if account_id == None:
+            if account_id == None :
                 message = "You're not authorized"
                 raise Exception
             try:
@@ -72,44 +72,112 @@ class imageHandler(tornado.web.RequestHandler):
         except:
             self.write(message)
 # /
+
     async def get(self):
         code = 4000
         status = False
         message = ""
         result = []
         try:
-            get_account_id = await SecureHeader.decrypt(self.request.headers['Authorization'])
-            if get_account_id == None:
-                code = 3241
-                status = False
-                message = "Invalid user Session"
-                raise Exception
-            try:
-                post_id = user_image_folder.find({})
 
-                async for i in post_id:
+            account_id = await SecureHeader.decrypt(self.request.headers["Authorization"])
+            if  account_id ==None:
+                code = 8765
+                status = False
+                message = "You're not authorized"
+                raise Exception
+
+            try:
+                imageList = user_image_folder.find()
+                async for i in imageList:
                     i['_id'] = str(i['_id'])
                     result.append(i)
-                code = 200
+                code = 2000
                 status = True
-                message = "Uploaded images:"
-                response = {
-                    "code": code,
-                    "status": status,
-                    "message": message,
-                    "result": result
-                }
-                self.write(response)
+                message = "List of images"
             except:
-                code = 3232
                 status = False
-                message = "Internal error!"
-                raise Exception
-        except:
+                if not len(message):
+                    code = 5623
+                    status = False
+                    message = 'Internal Error, Please Contact the Support Team.'
             response = {
-                "code": code,
-                "status": status,
-                "message": message,
-                "result": result
+                'code': code,
+                'status': status,
+                'message': message
+            }
+            try:
+                response['result'] = result
+                self.write(response)
+                self.finish()
+                return
+            except:
+                status = False
+                code = 5011
+                message = 'Internal Error, Please Contact the Support Team.'
+            response = {
+                'code': code,
+                'status': status,
+                'message': message
             }
             self.write(response)
+            self.finish()
+            return
+        except:
+            response = {
+                'code': code,
+                'status': status,
+                'message': message
+            }
+            self.write(response)
+            self.finish()
+            return
+
+    async def delete(self):
+        code = 4000
+        status = False
+        message = ""
+        result = []
+        try:
+            
+            try:
+                imageId = ObjectId(self.request.arguments["id"][0].decode())
+            except:
+                code = 3000
+                status = False
+                message = "invalid image id"
+                raise Exception
+            proUpdate = user_image_folder.delete_one(
+                {
+                    "_id": imageId
+                },
+            )
+            code = 2000
+            status = True
+            message = "Image has been removed"
+        except:
+            status = False
+            if not len(message):
+                message = 'Internal Error, Please Contact the Support Team.'
+        response = {
+            'code': code,
+            'status': status,
+            'message': message
+        }
+        try:
+            response['result'] = result
+            self.write(response)
+            self.finish()
+            return
+        except Exception as e:
+            status = False
+            code = 5011
+            message = 'Internal Error, Please Contact the Support Team.'
+            response = {
+                'code': code,
+                'status': status,
+                'message': message
+            }
+            self.write(response)
+            self.finish()
+            return
