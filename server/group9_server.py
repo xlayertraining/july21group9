a0fecb992f58c79885e0b89cecd219056422d3b0
Prithviraj Amin <prithvi@xlayer.in>
2021-08-14 09:03:11 +0000

    Restart, start, stop scripts added.

---
+++ server/group9_server.py
@@ -0,0 +1,28 @@
+from common_library import *
+from sign_up import signUpHandler
+from post import imageHandler
+from sign_in import signInHandler
+from post_like import PostLikeHandler
+from post_comment import PostCommentHandler
+from post_favourites import Postfavouriteshandler
+from search_news import SearchNewsHandler
+from user_profile import ProfileHandler
+from user_post import UserSharedPostHandler
+
+def app():
+    return tornado.web.Application([
+        (r"/julygroup9_web/api/sign/up", signUpHandler),
+        (r"/julygroup9_web/api/sign/in", signInHandler),
+        (r'/julygroup9_web/api/post', imageHandler),
+        (r'/julygroup9_web/api/post/like',PostLikeHandler),
+        (r'/julygroup9_web/api/post/comment',PostCommentHandler),
+        (r'/julygroup9_web/api/post/favourites',Postfavouriteshandler),
+        (r'/julygroup9_web/api/post/search',SearchNewsHandler),
+        (r'/julygroup9_web/api/profile',ProfileHandler),
+        (r'/julygroup9_web/api/post/user',UserSharedPostHandler)
+    ])
+
+
+if __name__ == "__main__":
+    app().listen(8009)
+    tornado.ioloop.IOLoop.current().start()
