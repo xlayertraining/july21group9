4867644dbb9e882545956b49c6d879216d239288
Basabjit Singha Ray <basabjitsingharay@gmail.com>
2021-08-28 19:53:18 +0530

    SignUp.dart api integration is completed

--- application/news_flix/lib/SignUp.dart
+++ application/news_flix/lib/SignUp.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:untitled2/SplashScreen.dart';
 import 'package:untitled2/Homepage.dart';
+import 'package:untitled2/config/configuration.dart';
 import 'package:untitled2/util/toast_util.dart';
 
 class SignUp extends StatelessWidget {
@@ -45,176 +48,182 @@ class _MyWidgetState extends State<MyWidget> {
     double height = MediaQuery.of(context).size.height;
 
     return WillPopScope(
-      onWillPop: () async {
-        return true;
-      },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-
-          backgroundColor: Colors.deepPurpleAccent,
-          iconTheme: IconThemeData(color: Colors.white),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.only(
-              top: 10,
-              right: 20,
-              left: 20,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-
-                  width: width ,
-                  height: height * 0.25,
-                  child: Lottie.asset(
-                    "assets/Sign Up.json",
-                    fit: BoxFit.contain,
-
-                  ),
-                ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: firstnameController,
-                  decoration: InputDecoration(
-                      labelText: "First name :",
-                      labelStyle:
-                          TextStyle(color: Colors.deepPurpleAccent.shade100),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurple),
-                        borderRadius: BorderRadius.circular(25.0),
+        onWillPop: () async {
+          return true;
+        },
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurpleAccent,
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                top: 10,
+                right: 20,
+                left: 20,
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: width,
+                      height: height * 0.25,
+                      child: Lottie.asset(
+                        "assets/Sign Up.json",
+                        fit: BoxFit.contain,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black87),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.black,
-                      )),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                    controller: lastnameController,
-                    decoration: InputDecoration(
-                        labelText: "Last name :",
-                        labelStyle:
-                            TextStyle(color: Colors.deepPurpleAccent.shade100),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple),
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87),
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        prefixIcon: Icon(Icons.person, color: Colors.black))),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                    controller: phoneController,
-                    decoration: InputDecoration(
-                      labelText: "Contact No.",
-                      labelStyle:
-                          TextStyle(color: Colors.deepPurpleAccent.shade100),
-                      prefixIcon: Icon(Icons.phone, color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurple),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black87),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    )),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      labelStyle:
-                          TextStyle(color: Colors.deepPurpleAccent.shade100),
-                      prefixIcon: Icon(Icons.mail, color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurple),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black87),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    )),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                    controller: passwordController,
-                    obscureText: !passShow,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle:
-                          TextStyle(color: Colors.deepPurpleAccent.shade100),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            passShow = !passShow;
-                          });
-                        },
-                        icon: (passShow == true)
-                            ? Icon(Icons.visibility, color: Colors.grey)
-                            : Icon(Icons.visibility_off, color: Colors.grey),
-                      ),
-                      prefixIcon: Icon(Icons.lock, color: Colors.black),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurple),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black87),
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    )),
-                SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    validate();
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(
-                      left: 50,
-                      right: 50,
-                      top: 8,
-                      bottom: 8,
                     ),
-                    // createAlbum("test string");
-                    child: Text(
-                      "Submit",
-                      style: TextStyle(color: Colors.white),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          controller: firstnameController,
+                          decoration: InputDecoration(
+                              labelText: "First name :",
+                              labelStyle: TextStyle(
+                                  color: Colors.deepPurpleAccent.shade100),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.deepPurple),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black87),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.black,
+                              )),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                            controller: lastnameController,
+                            decoration: InputDecoration(
+                                labelText: "Last name :",
+                                labelStyle: TextStyle(
+                                    color: Colors.deepPurpleAccent.shade100),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.deepPurple),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black87),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                prefixIcon:
+                                    Icon(Icons.person, color: Colors.black))),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                            controller: phoneController,
+                            decoration: InputDecoration(
+                              labelText: "Contact No.",
+                              labelStyle: TextStyle(
+                                  color: Colors.deepPurpleAccent.shade100),
+                              prefixIcon:
+                                  Icon(Icons.phone, color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.deepPurple),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black87),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                  color: Colors.deepPurpleAccent.shade100),
+                              prefixIcon: Icon(Icons.mail, color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.deepPurple),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black87),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                            controller: passwordController,
+                            obscureText: !passShow,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                  color: Colors.deepPurpleAccent.shade100),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    passShow = !passShow;
+                                  });
+                                },
+                                icon: (passShow == true)
+                                    ? Icon(Icons.visibility, color: Colors.grey)
+                                    : Icon(Icons.visibility_off,
+                                        color: Colors.grey),
+                              ),
+                              prefixIcon: Icon(Icons.lock, color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.deepPurple),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black87),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            validate();
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              left: 50,
+                              right: 50,
+                              top: 8,
+                              bottom: 8,
+                            ),
+                            // createAlbum("test string");
+                            child: Text(
+                              "Submit",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.deepPurple),
+                        ),
+                      ],
                     ),
-                  ),
-                  style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
-                ),
-              ],
+                  ]),
             ),
-          ]),
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 
   validate() async {
@@ -251,7 +260,35 @@ class _MyWidgetState extends State<MyWidget> {
       ToastUtil.error(_context!, message: "Enter your password.");
       return;
     }
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomePage()));
+    // Navigator.pushReplacement(
+    //     context, MaterialPageRoute(builder: (context) => HomePage()));
+    var response;
+    try {
+      response = await Dio().post(
+        Configuration.serverUrl + "/sign_up",
+        data: {
+          "firstName": firstnameController.text,
+          "lastName": lastnameController.text,
+          "emailAddress": emailController.text,
+          "phoneNumber": phoneController.text,
+          "password": passwordController.text
+        },
+      );
+    } catch (e, s) {
+      print(e.toString() + s.toString());
+    }
+    try {
+      print(response);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (response.data['status']) {
+     await prefs.setString(Configuration.signedInKey,response.data['result'][0]['Authorization']);
+     Configuration.authToken= response.data['result'][0]['Authorization'];
+     ToastUtil.info(_context!, message: response.data['message']);
+      } else {
+        ToastUtil.error(_context!, message: response.data['message']);
+      }
+    } catch (e, s) {
+      print(e.toString() + s.toString());
+    }
   }
 }
--- application/news_flix/lib/config/configuration.dart
+++ application/news_flix/lib/config/configuration.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 
 
 class Configuration {
-  static var serverUrl = "https://api.xlayer.in/julygroup9_web/api";
-  // static var serverUrl = "http://localhost:8009/julygroup9_web/api";
+  // static var serverUrl = "https://api.xlayer.in/julygroup9_web/api";
+   static var serverUrl = "http://10.0.2.2:8009/julygroup9_web/api";
   static var signedInKey = "KJHGLIUJGHL";
 
   static var authToken = '';
