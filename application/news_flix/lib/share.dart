6fe835b86c56810fd72dd6a45ef2e0d4f12b994a
Samyadeep Saha <sahasamyadeep@gmail.com>
2021-09-01 19:38:47 +0530

    profile2.dart is modified

---
+++ application/news_flix/lib/share.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Share extends StatelessWidget {
+  const Share({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 3,
+        backgroundColor: Colors.purpleAccent[78],
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          "Bookmark",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                Colors.red,
+                Colors.yellow,
+                Colors.blue,
+                Colors.purple
+              ])),
+        ),
+      ),
+    );
+  }
+}
