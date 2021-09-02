83cb1e584091d9bd198e0ec334822941adcbf1b4
Samyadeep Saha <sahasamyadeep@gmail.com>
2021-09-02 21:02:32 +0530

    user post API integration is added

--- application/news_flix/lib/my_news.dart
+++ application/news_flix/lib/my_news.dart
@@ -1,9 +1,12 @@
-
+import 'dart:async';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:untitled2/page_transition.dart';
 import 'package:rect_getter/rect_getter.dart';
+import 'package:untitled2/util/log_util.dart';
 
+import 'config/configuration.dart';
 import 'create_news.dart';
 
 class MyPost extends StatefulWidget {
@@ -14,33 +17,34 @@ class MyPost extends StatefulWidget {
 }
 
 class _MyPostState extends State<MyPost> {
-
   var controller;
-
   // final Duration animationDuration = Duration(milliseconds: 300);
   // final Duration delay = Duration(milliseconds: 300);
   // GlobalKey rectGetterKey = RectGetter.createGlobalKey();
   // late Rect rect;
 
-  void _onTap() async {
+  // void _onTap() async {
     // setState(() => rect = RectGetter.getRectFromKey(rectGetterKey)!); //<--onTap, update rect
-    Navigator.of(context).push(FadeRouteBuilder(child: AppImagePicker()));
-  }
+
+  // }
 
   @override
   Widget build(BuildContext context) {
+
     return
-    // Stack( //<-- Wrap Scaffold with a Stack
-    //   children: <Widget>[
-    Scaffold(
+        // Stack( //<-- Wrap Scaffold with a Stack
+        //   children: <Widget>[
+        Scaffold(
       appBar: AppBar(
         toolbarHeight: 60,
-        title: Text('My News',
+        title: Text(
+          'My News',
           style: TextStyle(
             color: Colors.deepPurple,
             fontSize: 24.0,
             fontStyle: FontStyle.italic,
-          ),),
+          ),
+        ),
         backgroundColor: Colors.white,
         elevation: 0.0,
         leading: IconButton(
@@ -53,26 +57,19 @@ class _MyPostState extends State<MyPost> {
       ),
       body: MyPostBody(),
       floatingActionButton:
-    // RectGetter(           //<-- Wrap Fab with RectGetter
-    // key: rectGetterKey,                       //<-- Passing the key
-    // child:
+          // RectGetter(           //<-- Wrap Fab with RectGetter
+          // key: rectGetterKey,                       //<-- Passing the key
+          // child:
           FloatingActionButton(
-            backgroundColor: Colors.deepPurple,
-            child: Icon(Icons.create_outlined),
-
+        backgroundColor: Colors.deepPurple,
+        child: Icon(Icons.create_outlined),
         onPressed: () {
-          _onTap();
-          // Navigator.push(
-          //           context,
-          //           MaterialPageRoute(
-          //           builder: (context) => AppImagePicker()));
+          // _onTap();
+          Navigator.of(context).push(FadeRouteBuilder(child: AppImagePicker()));
         },
       ),
-
-      );
+    );
   }
-
-
 }
 
 class MyPostBody extends StatefulWidget {
@@ -83,51 +80,92 @@ class MyPostBody extends StatefulWidget {
 }
 
 class _MyPostBodyState extends State<MyPostBody> {
-  List<String> images = [
-    "assets/boy.png",
-    "assets/boy.png",
-    "assets/boy.png",
-    "assets/boy.png",
-    "assets/boy.png",
-    "assets/boy.png",
-  ];
+  var userNews = [];
+  BuildContext? _context;
   @override
   Widget build(BuildContext context) {
+    if (_context == null) {
+      _context = context;
+      getuserNews();
+    }
     return Scaffold(
       body: ListView.builder(
         itemBuilder: (BuildContext, index) {
           return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://images.wallpapersden.com/image/download/demon-slayer-tanjirou-kamado_a2tsa2uUmZqaraWkpJRnamtlrWZlbWU.jpg'),
-                // backgroundImage: AssetImage(images[index]),
+              child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.wallpapersden.com/image/download/demon-slayer-tanjirou-kamado_a2tsa2uUmZqaraWkpJRnamtlrWZlbWU.jpg'),
+              // backgroundImage: AssetImage(images[index]),
+            ),
+            title: Text(
+              userNews[index]['title'].toString(),
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+                subtitle: Text(userNews[index]['description'].toString(),),
+                 trailing: IconButton(
+              icon: Icon(
+                Icons.delete,
+                color: Colors.red,
+                size: 22,
               ),
-
-              // title: Text("This is Post Title"),
-              // subtitle: Text("This is Post Description"),
-              title: Text(
-                'Samyadeep',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Samyadeep@gmail.com'),
-
-              trailing: IconButton(
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                  size: 22,
-                ),
-                onPressed: () {},
-              ),
-            )
-            );
+              onPressed: () {},
+            ),
+          ));
         },
-        itemCount: images.length,
+        itemCount: userNews.length,
         shrinkWrap: true,
         padding: EdgeInsets.all(5),
         scrollDirection: Axis.vertical,
       ),
+    );
+  }
+
+  getuserNews() async {
+    Response? resp = null;
+    resp = await Dio().get(Configuration.serverUrl + '/post/user',
+        options: Options(
+            headers: {'Authorization': ' Bearer ' + Configuration.authToken}));
+    print(resp);
+    try {
+      if (resp.data['status']){
+        userNews = resp.data['result'];
+        Log.i('0_length', userNews.length.toString());
+      }
+
+    } catch (e, s) {
+      print(e.toString() + s.toString());
+    }
+
+    Timer(
+      Duration(seconds: 1),
+      () {
+        setState(() {});
+      },
+    );
+  }
+  // this delete post is not functional.....
+  deluserNews() async {
+    Response? resp = null;
+    resp = await Dio().get(Configuration.serverUrl + '/delete',
+        options: Options(
+            headers: {'Authorization': ' Bearer ' + Configuration.authToken}));
+    print(resp);
+    try {
+      if (resp.data['status']){
+        userNews = resp.data['result'];
+        Log.i('0_length', userNews.length.toString());
+      }
+
+    } catch (e, s) {
+      print(e.toString() + s.toString());
+    }
+
+    Timer(
+      Duration(seconds: 1),
+          () {
+        setState(() {});
+      },
     );
   }
 }
@@ -135,11 +173,12 @@ class _MyPostBodyState extends State<MyPostBody> {
 class FadeRouteBuilder<T> extends PageRouteBuilder<T> {
   final Widget child;
 
-  FadeRouteBuilder({required this.child, })
-      : super(
-    pageBuilder: (context, animation1, animation2) => child,
-    transitionsBuilder: (context, animation1, animation2, child) {
-      return FadeTransition(opacity: animation1, child: child);
-    },
-  );
+  FadeRouteBuilder({
+    required this.child,
+  }) : super(
+          pageBuilder: (context, animation1, animation2) => child,
+          transitionsBuilder: (context, animation1, animation2, child) {
+            return FadeTransition(opacity: animation1, child: child);
+          },
+        );
 }
