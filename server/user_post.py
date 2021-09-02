66ebdf2376cdac45a3e9f48ea20831a9d93eb7a2
Aniket_lodh <aniket.lodh.08@gmail.com>
2021-09-02 21:13:30 +0530

    fixed a bug

--- server/user_post.py
+++ server/user_post.py
@@ -67,3 +67,68 @@ class UserSharedPostHandler(tornado.web.RequestHandler):
             self.write(response)
             self.finish()
             return
+    
+    async def delete(self):
+        code = 4000
+        status = False
+        message = ""
+        result = []
+        try:
+
+            account_id = await SecureHeader.decrypt(self.request.headers["Authorization"])
+            if account_id == None:
+                code = 8765
+                status = False
+                message = "You're not authorized"
+                raise Exception
+            # NewsId
+            try:
+                newsId = ObjectId(self.request.arguments["newsId"][0].decode())
+            except:
+                code = 3000
+                status = False
+                message = "Invalid news id"
+                raise Exception
+            proUpdate = await user_news_folder.delete_one(
+                {
+                    "_id": newsId
+                },
+            )
+            code = 2000
+            status = True
+            message = "News deleted Successfully."
+        except:
+            code = 6754
+            status = False
+            message = 'Internal Error, Please Contact the Support Team.'
+            raise Exception
+
+        try:
+            response = {
+                'code': code,
+                'status': status,
+                'message': message,
+                "result": result
+            }
+            self.write(response)
+            self.finish()
+            return
+        except Exception as e:
+            template = 'Exception: {0}. Argument: {1!r}'
+            iMessage = template.format(type(e).__name__, e.args)
+            message = 'Internal Error, Please Contact the Support Team.'
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = exc_tb.tb_frame.f_code.co_filename
+            print('EXC', iMessage)
+            print('EX2', 'FILE: ' + str(fname) + ' LINE: ' + str(exc_tb.tb_lineno) + ' TYPE: ' + str(exc_type))
+
+        response = {
+            'code': code,
+            'status': status,
+            'message': message,
+            "result": result
+        }
+        self.write(response)
+        self.finish()
+        return
+        
