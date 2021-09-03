21b9e58bbe69683443c5f4dc51a9f162e260e477
Samyadeep Saha <sahasamyadeep@gmail.com>
2021-09-03 13:22:52 +0530

    new comment_two.dart is added

--- application/news_flix/lib/full_view.dart
+++ application/news_flix/lib/full_view.dart
@@ -30,49 +30,93 @@ class _FullViewState extends State<FullView> {
         backgroundColor: Colors.white,
         toolbarHeight: 70,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-            color: Colors.white,
-            margin: EdgeInsets.only(left: 8,right: 8),
-            height: 700,
-            padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 80),
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              Text(
-                'Title :',
-            textDirection: TextDirection.ltr,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  fontSize: 20.0,
+      body:
+      ListView(children: <Widget>[
+      Container(
+      margin: EdgeInsets.all(8.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: InkWell(
+          onTap: () => print("ciao"),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+                child: Image.asset("assets/anime wallpaper1.jpg",
+                    width: 300, height: 150, fit: BoxFit.fill),
+              ),
+              ListTile(
+                title: Text('Title 1 :',
+                  style: TextStyle(
+                  fontSize: 18.0,
                   color: Colors.deepPurple,
                   fontWeight: FontWeight.bold,
                   fontStyle: FontStyle.italic),
-            ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width / 2,
-                child: Image.network(
-                  'https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-old-paper-texture-parchment-background-image_354980.jpg',
-                  fit: BoxFit.fitWidth,
                 ),
+                subtitle: Text('Description 1 :',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                  ),),
+                selected: true,
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'description :',
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              ),
-            ])),
+            ],
+          ),
+        ),
       ),
+    ),
+    ]
+    )
     );
   }
 }
+
+//       SingleChildScrollView(
+//         child: Container(
+//             color: Colors.white,
+//             margin: EdgeInsets.only(left: 8,right: 8),
+//             height: 700,
+//             padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 80),
+//             child: Column(mainAxisSize: MainAxisSize.min, children: [
+//               Text(
+//                 'Title :',
+//             textDirection: TextDirection.ltr,
+//               textAlign: TextAlign.start,
+//               style: TextStyle(
+//                   fontSize: 20.0,
+//                   color: Colors.deepPurple,
+//                   fontWeight: FontWeight.bold,
+//                   fontStyle: FontStyle.italic),
+//             ),
+//               SizedBox(
+//                 width: 10,
+//               ),
+//               Container(
+//                 width: MediaQuery.of(context).size.width,
+//                 height: MediaQuery.of(context).size.width / 2,
+//                 child: Image.network(
+//                   'https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-old-paper-texture-parchment-background-image_354980.jpg',
+//                   fit: BoxFit.fitWidth,
+//                 ),
+//               ),
+//               SizedBox(
+//                 width: 10,
+//               ),
+//               Text(
+//                 'description :',
+//                 textDirection: TextDirection.ltr,
+//                 textAlign: TextAlign.start,
+//                 style: TextStyle(
+//                     fontSize: 20.0,
+//                     color: Colors.deepPurple,
+//                     fontWeight: FontWeight.bold,
+//                     fontStyle: FontStyle.italic),
+//               ),
+//             ])),
+//       ),
+//     );
+
