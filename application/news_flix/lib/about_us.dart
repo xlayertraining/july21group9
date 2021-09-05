e0fe32b7647a607c26f190f23bb4bce4d66ef69f
Basabjit Singha Ray <basabjitsingharay@gmail.com>
2021-09-05 14:49:36 +0530

    about_us.dart page

--- application/news_flix/lib/about_us.dart
+++ application/news_flix/lib/about_us.dart
@@ -2,36 +2,68 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AboutUs extends StatelessWidget {
+
+
   const AboutUs({Key? key}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
 
+    return Scaffold(
+      backgroundColor: Colors.grey.shade400,
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_sharp),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         elevation: 3,
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.deepPurple,
         iconTheme: IconThemeData(color: Colors.white),
         title: Text(
           "About us",
           style: TextStyle(
               color: Colors.white,
-              fontSize: 24.0,
+              fontSize: 25.0,
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [Colors.purple, Colors.blue]
-              )
-          ),
+      body:Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('FrontEnd Team :',style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold)),
+            SizedBox(height: 40),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                'assets/samyadeep.jpg',
+              ),
+            ),
+            SizedBox(height: 10),
+            Text('Samyadeep Saha'),
+            SizedBox(height: 90),
+            Text('BackEnd Team :',style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold)),
+            SizedBox(height: 40),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                'assets/aniket.jpg',
+              ),
+            ),
+
+            SizedBox(height: 10),
+            Text('Aniket Lodh'),
+            SizedBox(height: 40),
+
+          ],
         ),
-      ),
-    );
+
+
+      );
   }
 }
